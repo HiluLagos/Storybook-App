5c11a6404f76f025ab93ac6a6f7572bd64a9359a
Tomibernardez04 <tbernardez@mail.austral.edu.ar>
2024-10-21 18:38:57 -0300

    Colors implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,66 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          100: '#D0F2B5',
+          300: '#C1ED9C',
+          500: '#B1E983',
+          700: '#8EBA69',
+          900: '#6A8C4F',
+        },
+        'secondary': {
+          100: '#F4F3E1',
+          300: '#EFEED5',
+          500: '#E8E6C3',
+          700: '#D1CFB0',
+          900: '#BAB89C',
+        },
+        'tertiary': {
+          100: '#FFFFFF',
+          300: '#E6E6E6',
+          500: '#CCCCCC',
+          700: '#B3B3B3',
+          900: '#999999',
+        },
+        'bg': '#F4F3E1',
+        'utility': {
+          'button': '#95ADE5',
+          'pressed-button': '#596889',
+          'loading-button': '#778ab7',
+          'error': '#F10700',
+          'border': '#000000',
+        },
+        'icon': {
+          'lightblue': '#2DBCD2',
+          'orange': '#EDB14D',
+          'lightgreen': '#74CB3E',
+          'yellow': '#E0E030',
+          'green': '#28C40B',
+          'violet': '#D74DDE',
+        },
+        'notification': {
+          'bg': '#FBF0FC',
+          'warning': {
+            'primary': '#FF0043',
+            'bg': '#FDF1F5',
+          },
+          'activity': {
+            'bg': '#EEFBF2',
+          },
+          'task': {
+            'bg': '#FCF7F0',
+          },
+          'success': {
+            'bg': '#EEFBF2',
+          },
+          'info': {
+            'bg': '#E4FAFA',
+          },
+        }
+      }
+    },
   },
   plugins: [],
 }
