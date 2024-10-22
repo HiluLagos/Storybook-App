da86d8d31db1552cfca8156828d82711269843cb
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-22 19:59:51 -0300

    Still working out with the lock.

--- src/components/icon/utility/lock/Lock.stories.ts
+++ src/components/icon/utility/lock/Lock.stories.ts
@@ -1,28 +1,33 @@
-import {Meta, StoryObj} from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import Lock from "./Lock";
 
 const meta = {
   title: 'Icon/Utility/lock',
   component: Lock,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-
+    fill: {
+      control: 'select',
+      options: ['filled', 'outline'],
+      description: 'The fill state of the lock',
+    },
   },
 } satisfies Meta<typeof Lock>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Filled: Story = {
+  args: {
+    fill: 'filled',
+  },
+};
 
-}
-
-export const Pressed: Story = {
-
-}
+export const Outline: Story = {
+  args: {
+    fill: 'outline',
+  },
+};
--- src/components/icon/utility/lock/Lock.tsx
+++ src/components/icon/utility/lock/Lock.tsx
@@ -1,36 +1,31 @@
 import React from "react";
-import {cva, VariantProps} from "class-variance-authority";
+import { cva, VariantProps } from "class-variance-authority";
 
 const lock = cva(
   "", {
     variants: {
-      state: {
+      fill: {
         filled: "",
-        outline: "fill-none",
+        outline: "",
       }
+    },
+    defaultVariants: {
+      fill: "outline",
     }
   }
-)
+);
 
 type LockProps = VariantProps<typeof lock>;
 
-const Lock: React.FC<LockProps> = ({state}) => {
+const Lock: React.FC<LockProps> = ({ fill = "outline" }) => {
   return (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" id="Lock--Streamline-Unicons" height="24"
-         width="24">
-      <desc>Lock Streamline Icon: https://streamlinehq.com</desc>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+         className="size-6">
 
-      <path
-        className={lock({state})}
-        d="M4.625 12c0-0.8 0.309-1.568 0.855-2.115C6.028 9.339 6.797 9.03 7.596 9.03h8.808c0.8 0 1.568 0.31 2.115 0.855 0.547 0.547 0.856 1.316 0.856 2.115v8.232c0 0.8-0.31 1.568-0.856 2.115-0.547 0.547-1.315 0.855-2.115 0.855H7.596c-0.8 0-1.568-0.309-2.115-0.855-0.546-0.547-0.855-1.316-0.855-2.115V12z"
-        fill="#000000" stroke-width="1">
-      </path>
-      <path
-        d="M17.88 8.472V6.12c0 -1.5595 -0.6195 -3.0551 -1.7222 -4.1578S13.5595 0.24 12 0.24c-1.5595 0 -3.0551 0.6195 -4.1578 1.7222S6.12 4.5605 6.12 6.12v2.352h2.352V6.12c0 -0.9357 0.3717 -1.833 1.0333 -2.4947C10.167 2.9637 11.0644 2.592 12 2.592s1.833 0.3717 2.4947 1.0333c0.6616 0.6617 1.0333 1.559 1.0333 2.4947v2.352h2.352Z"
-        fill="#000000" stroke-width="1">
-      </path>
+      <path stroke-linecap="round" stroke-linejoin="round"
+            d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"/>
     </svg>
   )
-}
+};
 
 export default Lock;
