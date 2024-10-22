d854c27e28a665c83f8281816bd0473da2924a9e
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-10-22 19:28:27 -0300

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,6 @@ module.exports = {
           700: '#B3B3B3',
           900: '#999999',
         },
-        'bg': '#F4F3E1',
         'utility': {
           'button': '#95ADE5',
           'pressed-button': '#596889',
@@ -44,26 +43,85 @@ module.exports = {
           'green': '#28C40B',
           'violet': '#D74DDE',
         },
-        'notification': {
-          'bg': '#FBF0FC',
-          'warning': {
-            'primary': '#FF0043',
-            'bg': '#FDF1F5',
+        'shadow': {
+          'black':{
+            60: 'rgba(0, 0, 0, 0.06)',
+            40 : 'rgba(0, 0, 0, 0.04)',
           },
-          'activity': {
-            'bg': '#EEFBF2',
+          'green':{
+            25: 'rgba(40, 196, 11, 0.6)',
           },
-          'task': {
-            'bg': '#FCF7F0',
+        },
+        'bg': {
+          'default': '#F4F3E1',
+          'bright':{
+            'blue': '#E4FAFA',
+            'orange': '#FCF7F0',
+            'green': '#EEFBF2',
+            'violet': '#FBF0FC',
+            'red': '#FDF1F5',
           },
-          'success': {
-            'bg': '#EEFBF2',
+          'colorful':{
+            'lightblue': '#2DBCD2',
+            'orange': '#EDB14D',
+            'lightgreen': '#74CB3E',
+            'violet': '#D74DDE',
+            'red': '#F10700',
+            'yellow': '#E0E030',
           },
-          'info': {
-            'bg': '#E4FAFA',
+        },
+        'text': {
+          'default': {
+            'black':'#000000',
+            'white': '#FFFFFF',
           },
-        }
-      }
+          'description': {
+            'dark': '#707070',
+            'bright': '#A8A8A8',
+          },
+          'colorful':{
+              'orange': '#EDB14D',
+              'lightgreen': '#74CB3E',
+              'violet': '#D74DDE',
+          },
+          'dark':{
+            'blue': '#1D7987',
+            'orange': '#87652C',
+            'green': '#135E05',
+            'violet': '#880071',
+            'yellow': '#7A7A1A',
+          },
+          'primary': {
+            100: '#D0F2B5',
+            900: '#6A8C4F',
+          },
+          'secondary': {
+            100: '#F4F3E1',
+          },
+          'button': {
+            'pressed': '#596889',
+
+          },
+        },
+      },
+      backgroundImage:{
+        'blue': {
+          100: 'linear-gradient(to right, #33D4ED, #1D7987)',
+          30: 'linear-gradient(to right, rgba(51, 212, 237, 0.3), rgba(29, 121, 135, 0.3))',
+        },
+        'green': {
+          100: 'linear-gradient(to right, #28C40B, #135E05)',
+          30: 'linear-gradient(to right, rgba(40, 196, 11, 0.3), rgba(19, 94, 5, 0.3))',
+        },
+        'orange': {
+          100: 'linear-gradient(to right, #EDB14D, #87652C)',
+          30: 'linear-gradient(to right, rgba(237, 177, 77, 0.3), rgba(135, 101, 44, 0.3))',
+        },
+        'yellow': {
+          100: 'linear-gradient(to right, #E0E030, #7A7A1A)',
+          30: 'linear-gradient(to right, rgba(224, 224, 48, 0.3), rgba(122, 122, 26, 0.3))',
+        },
+      },
     },
   },
   plugins: [],
