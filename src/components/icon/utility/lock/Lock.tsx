27cd284940c90291ce1a6952feab39babd3ce228
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-27 18:33:09 -0300

    I had to learn how to read svg's path to make this icon.

--- src/components/icon/utility/lock/Lock.tsx
+++ src/components/icon/utility/lock/Lock.tsx
@@ -5,8 +5,8 @@ const lock = cva(
   "", {
     variants: {
       fill: {
-        filled: "",
-        outline: "",
+        filled: "currentColor",
+        outline: "none",
       }
     },
     defaultVariants: {
@@ -21,9 +21,11 @@ const Lock: React.FC<LockProps> = ({ fill = "outline" }) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
          className="size-6">
-
       <path stroke-linecap="round" stroke-linejoin="round"
-            d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"/>
+            d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75"/>
+
+      <path stroke-linecap="round" stroke-linejoin="round" fill={lock({ fill })}
+            d="M7.5 10.5m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"/>
     </svg>
   )
 };
