257203fc7a8469484f3fa6d71cfbd52f162d9ea8
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-27 19:12:05 -0300

    Loading con created.

---
+++ src/components/icon/utility/loading/Loading.stories.ts
@@ -0,0 +1,34 @@
+import {Meta, StoryObj} from "@storybook/react";
+import Loading from "./Loading.tsx";
+
+const meta = {
+  title: 'Icon/Utility/loading',
+  component: Loading,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: ['default', 'primary'],
+      },
+    },
+  }
+} satisfies Meta<typeof Loading>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    color: 'default',
+  },
+};
+
+export const Primary: Story = {
+  args: {
+    color: 'primary',
+  },
+};
