c3f682481974cf864ea3e2e0160c6393def8c7cc
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-28 12:03:51 -0300

    Added a tweaked shadow to the progress bar.

--- src/components/progress/bar/Bar.tsx
+++ src/components/progress/bar/Bar.tsx
@@ -7,7 +7,7 @@ type BarProps = {
 const Bar: React.FC<BarProps> = ({percentage = 45}:BarProps) => {
 
   return (
-    <div className="bg-primary-100 w-[320px] h-[72px] rounded-2xl relative overflow-hidden">
+    <div className="bg-primary-100 w-[320px] h-[72px] rounded-2xl relative overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.3)]">
       <div className="bg-bg-colorful-lightgreen rounded-2xl absolute top-0 left-0" style={{width: `${percentage}%`, height: '100%'}}></div>
     </div>
   )
