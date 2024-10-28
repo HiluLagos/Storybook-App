9986ea1341e5a2ff813bcfc56e5c739629bf12e0
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-28 11:53:19 -0300

    Top Notch element created.

---
+++ src/components/icon/settings/drawer/topNotch/TopNotch.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TopNotch: React.FC = () => {
+  return (
+    <svg width="66" height="6" viewBox="0 0 66 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="1" y="1" width="64" height="4" rx="2" fill="#E6E6E6"/>
+      <rect x="1" y="1" width="64" height="4" rx="2" stroke="#E6E6E6" stroke-width="2" stroke-linejoin="round"/>
+    </svg>
+  )
+};
+
+export default TopNotch;
