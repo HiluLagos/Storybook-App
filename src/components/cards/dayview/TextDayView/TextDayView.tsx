04cce3f76c3249c0a657f63abe9b56bbd296812b
ezequielrickert <erickert@mail.austral.edu.ar>
2024-10-29 19:30:50 -0300

    Finished DayView

---
+++ src/components/cards/dayview/TextDayView/TextDayView.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import DayView from "../DayView/DayView.tsx";
+import Typography from "../../../typography/Typography.tsx";
+
+type DayViewProps = {
+    header: string,
+    children: string,
+    tag: "missed" | "done" | "info",
+    type: "activity" | "task",
+    time: string
+}
+
+const TextDayView: React.FC<DayViewProps> = ({header, children, tag, type, time}: DayViewProps) => {
+    return(
+        <DayView type={type} time={time} header={header} tag={tag}>
+            <Typography size="m" weight="regular">{children}</Typography>
+        </DayView>
+    )
+}
+
+export default TextDayView;
