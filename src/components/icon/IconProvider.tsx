2a75f3e4a1166a1a956b5a564188a69879dd5436
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-29 17:46:06 -0300

    Added more icons to IconProvider.

--- src/components/icon/IconProvider.tsx
+++ src/components/icon/IconProvider.tsx
@@ -4,10 +4,13 @@ import Pills from "./activity/pills/Pills.tsx";
 import Salad from "./activity/salad/Salad.tsx";
 import Steps from "./activity/steps/Steps.tsx";
 import Water from "./activity/water/Water.tsx";
+import Task from "./menu/Task/Task.tsx";
+import Log from "./menu/Log/Log.tsx";
+import Activities from "./menu/Activities/Activities.tsx";
 
 type IconProviderProps = {
   size: "medium" | "large";
-  activity: "pills" | "salad" | "steps" | "water";
+  activity: "pills" | "salad" | "steps" | "water" | "task" | "log" | "activities";
 };
 
 const iconMap = {
@@ -15,6 +18,9 @@ const iconMap = {
   salad: Salad,
   steps: Steps,
   water: Water,
+  task: Task,
+  log: Log,
+  activities: Activities,
 };
 
 const IconProvider: React.FC<IconProviderProps> = ({ size, activity }: IconProviderProps) => {
