536f940a82325d448059a88a7cc7ddbd4f7a7e57
Tomibernardez04 <tbernardez@mail.austral.edu.ar>
2024-10-29 22:23:58 -0300

    first iteration for dropdown and its stories

--- src/components/menu/dropdown/dropdown/Dropdown.tsx
+++ src/components/menu/dropdown/dropdown/Dropdown.tsx
@@ -1,31 +1,39 @@
-import React from "react";
-import {cva, VariantProps} from "class-variance-authority";
+import React, { useState, useEffect } from 'react';
+import { CheckList, CheckListProps } from '../../../button/utility/checklist/CheckList';
 
-const dropdown = cva(
-  "rounded-2xl w-80", {
-    variants: {
-      dropped: {
-        true: "bg-red-800",
-        false: "bg-gray-700",
-      }
-    },
-    defaultVariants: {
-      dropped: false,
-    }
-  }
-)
-
-type DropdownProps = VariantProps<typeof dropdown> & {
-  children: React.ReactNode;
+interface DropdownProps {
+    checkLists: Array<CheckListProps>;
+    title: string;
+    dropped: boolean;
 }
 
-export const Dropdown: React.FC<DropdownProps> = ({ dropped = false, children }: DropdownProps) => {
-  console.log(dropdown({ dropped }));
+export const Dropdown: React.FC<DropdownProps> = ({ checkLists, title, dropped }) => {
+    const [isOpen, setIsOpen] = useState(dropped);
 
-  return (
-    <div className={dropdown({ dropped })}>
-      <h1>Dropdown</h1>
-      { dropped ? children : null }
-    </div>
-  )
-}
+    // Sincroniza el estado interno de isOpen cuando cambia la prop dropped
+    useEffect(() => {
+        setIsOpen(dropped);
+    }, [dropped]);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="w-80 p-2 border border-gray-300 rounded-lg">
+            <div className="cursor-pointer" onClick={toggleDropdown}>
+                {title}
+            </div>
+
+            {isOpen && checkLists.length > 0 && (
+                <div className="mt-2  w-80">
+                    {checkLists.map((checkList, index) => (
+                        <div key={index} className="mb-2">
+                            <CheckList {...checkList} />
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
