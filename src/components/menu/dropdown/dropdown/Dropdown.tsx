607ad919aa015def193e542a20aed4c26b1ab438
Tomibernardez04 <tbernardez@mail.austral.edu.ar>
2024-10-31 12:13:53 -0300

    div principal de titulo centrado; correcion de centrado de checklists; correcion de mb-2 al final de checklists

--- src/components/menu/dropdown/dropdown/Dropdown.tsx
+++ src/components/menu/dropdown/dropdown/Dropdown.tsx
@@ -21,8 +21,8 @@ export const Dropdown: React.FC<DropdownProps> = ({ checkLists, title, dropped, 
     };
 
     return (
-        <div className={`w-[328px] border-2 border-primary-100 rounded-2xl p-2 ${isOpen ? '' : 'h-10'} transition-all`}>
-            <div className="flex justify-between items-center w-[308px] h-[24px] p-2 cursor-pointer" onClick={toggleDropdown}>
+        <div className={`w-[328px] border-2 border-primary-100 rounded-2xl p-2 h-fit transition-all`}>
+            <div className="flex justify-between items-center w-[308px] h-[24px] p-2 cursor-pointer">
                 <div className="flex items-center h-full">
                     <Arrow orientation={isOpen ? 'down' : 'right'} state="default" onToggleAction={toggleDropdown}/>
                     <span className="ml-2 text-base font-medium">{title}</span>
@@ -30,20 +30,19 @@ export const Dropdown: React.FC<DropdownProps> = ({ checkLists, title, dropped, 
                 <span className="ml-2 text-primary-900 text-base font-medium">{topRightText}</span>
             </div>
 
-            {isOpen && (
-                <div className="w-[308px]">
-                    <div className="border-b-2 border-primary-700 my-2"/>
-                </div>
-            )}
-
             {isOpen && checkLists.length > 0 && (
-                <div className="w-full">
-                    {checkLists.map((checkList, index) => (
-                        <div key={index} className="mb-2">
-                            <CheckList {...checkList} />
-                        </div>
-                    ))}
-                </div>
+                <>
+                    <div className="w-[308px]">
+                        <div className="border-b-2 border-primary-700 my-2"/>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        {checkLists.map((checkList, index) => (
+                            <div key={index} className={`${index !== checkLists.length - 1 ? 'mb-2' : ''} w-fit`}>
+                                <CheckList {...checkList} />
+                            </div>
+                        ))}
+                    </div>
+                </>
             )}
         </div>
     );
