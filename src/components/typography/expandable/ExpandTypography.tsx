d51761bbb42db4191d7b9b95ac5c557aaa2b8d60
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2024-11-05 15:32:07 -0300

    Enhanced ExpandTypography changed limit

--- src/components/typography/expandable/ExpandTypography.tsx
+++ src/components/typography/expandable/ExpandTypography.tsx
@@ -14,7 +14,7 @@ const ExpandTypography: React.FC<ExpandTypographyProps> = ({weight = "regular", 
     setExpanded(!expanded);
   }
 
-  const limit: number = 58;
+  const limit: number = 56;
   const hasReadMore: boolean = children.length > limit;
 
     return (
