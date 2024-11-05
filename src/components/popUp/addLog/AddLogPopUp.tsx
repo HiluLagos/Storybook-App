4885b9d6fa4ef20d1cbc3fef62e04834b3e6228c
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-11-05 15:31:37 -0300

    pop up add log +fondo storybook

--- src/components/popUp/addLog/AddLogPopUp.tsx
+++ src/components/popUp/addLog/AddLogPopUp.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Cross from "../../icon/utility/cross/Cross.tsx";
 import AddLogPopUpIcon from "../../icon/popUps/addLog/AddLogPopUpIcon.tsx";
 import Typography from "../../typography/basic/Typography.tsx";
+import {Button} from "../../button/utility/button/Button.tsx";
 
 const AddLogPopUp: React.FC = () => {
     return (
-        <div className={"flex flex-col w-[288px] h-[271px] rounded-2xl p-5 space-y-6"}>
+        <div className={"flex flex-col w-[288px] h-[271px] rounded-2xl p-5 space-y-6 bg-bg-default"}>
             <div className={"self-end"}>
                 <Cross/>
             </div>
@@ -17,8 +18,7 @@ const AddLogPopUp: React.FC = () => {
                 </div>
             </div>
             <div className={"space-y-2 flex flex-col items-center justify-center"}>
-                <button className={" bg-bg-light-yellow rounded-lg w-[80px] h-[32px] text-text-description-dark"}>Add
-                </button>
+                <Button intent={"default"} size={"small"} value={"Add"}/>
             </div>
         </div>
     );
