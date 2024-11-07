81c0e76954abcd38dd9a014e41d7eed76c6aa5a6
Tomibernardez04 <tbernardez@mail.austral.edu.ar>
2024-11-07 12:18:16 -0300

    preference stories updated, new stories

--- src/components/cards/preferences/Preference.stories.ts
+++ src/components/cards/preferences/Preference.stories.ts
@@ -25,16 +25,58 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const PillDefault: Story = {
   args: {
     state: "default",
     activity: "pills",
   },
 };
 
-export const Dragged: Story = {
+export const PillDragged: Story = {
   args: {
     state: "dragged",
     activity: "pills",
+  },
+};
+
+export const SaladDefault: Story = {
+  args: {
+    state: "default",
+    activity: "salad",
+  },
+};
+
+export const SaladDragged: Story = {
+  args: {
+    state: "dragged",
+    activity: "salad",
+  },
+};
+
+export const StepsDefault: Story = {
+  args: {
+    state: "default",
+    activity: "steps",
+  },
+};
+
+export const StepsDragged: Story = {
+  args: {
+    state: "dragged",
+    activity: "steps",
+  },
+};
+
+export const WaterDefault: Story = {
+  args: {
+    state: "default",
+    activity: "water",
+  },
+};
+
+export const WaterDragged: Story = {
+  args: {
+    state: "dragged",
+    activity: "water",
   },
 };
