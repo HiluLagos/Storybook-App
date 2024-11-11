0e2d38f2b7cbffeead4d24342536fd0368e8f91d
Tomibernardez04 <tbernardez@mail.austral.edu.ar>
2024-11-11 18:14:53 -0300

    lock icon added to icon provider; Login inputs updated;

--- src/components/icon/IconProvider.tsx
+++ src/components/icon/IconProvider.tsx
@@ -1,4 +1,3 @@
-// src/components/icon/activity/IconProvider.ts
 import React from "react";
 import Pills from "./activity/pills/Pills.tsx";
 import Salad from "./activity/salad/Salad.tsx";
@@ -10,11 +9,11 @@ import Saved from "./menu/Saved/Saved.tsx";
 import Activities from "./menu/Activities/Activities.tsx";
 import Mail from "./utility/mail/Mail.tsx";
 import Confetti from "./popUps/confetti/Confetti.tsx";
-//import Lock from "./tech/log/lock/LockIcon.tsx";
+import Lock from "./tech/log/lock/LoginLockIcon.tsx";
 
 export type IconInterface = {
   size: "medium" | "large";
-  variant: "pills" | "salad" | "steps" | "water" | "task" | "log" | "activities" | "mail" | "saved" | "confetti"; //lock
+  variant: "pills" | "salad" | "steps" | "water" | "task" | "log" | "activities" | "mail" | "saved" | "confetti" | "lock";
 };
 
 const iconMap = {
@@ -28,12 +27,12 @@ const iconMap = {
   activities: Activities,
   mail: Mail,
   confetti: Confetti,
-  //lock: Lock
+  lock: Lock
 };
 
 const IconProvider: React.FC<IconInterface> = ({ size, variant }: IconInterface) => {
   const IconComponent = iconMap[variant];
-  return IconComponent ? <IconComponent size={size} /> : null;
+  return IconComponent ? <IconComponent size={size}/> : null;
 };
 
 export default IconProvider;
