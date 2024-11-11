84a46452b0e4f480aec8f37e460036e5b67a346e
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-11-11 14:32:46 -0300

    tweak

--- src/components/menu/utility/core/ActivityCore.tsx
+++ src/components/menu/utility/core/ActivityCore.tsx
@@ -18,13 +18,13 @@ interface ActivityCoreProps {
 }
 
 const ActivityCore: React.FC<ActivityCoreProps> = ({activity = "water", count = 159, max = 571, isCounter= true, children}) => {
-  const perTemp = (count * 100) / max;
+  const percentage = (count * 100) / max;
 
   return (
     <div className={"flex flex-col items-center"}>
       <Typography size={"h3"} weight={"semiBold"}>{textMap[activity]}</Typography>
       <div className={"max-h-[571px] flex flex-col items-center overflow-scroll space-y-4"} style={{ msOverflowStyle: "none", scrollbarWidth: "none" }}>
-        <CircularProgressComponent activity={activity} size={"large"} percentage={perTemp}/>
+        <CircularProgressComponent activity={activity} size={"large"} percentage={percentage}/>
         {children}
       </div>
       <div className={"pt-2"}>
