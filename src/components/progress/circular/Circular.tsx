68caa4d1295a652cb0e1b7ddf6e65ae66cbf85b3
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-11-12 18:57:56 -0300

    gradient

--- src/components/progress/circular/Circular.tsx
+++ src/components/progress/circular/Circular.tsx
@@ -36,6 +36,7 @@ const innerCircle = cva(
 )
 
 type CircularProps = VariantProps<typeof circular> & {
+  activity: "pills" | "salad" | "steps" | "water";
   percentage: number;
   children?: React.ReactNode;
 };
@@ -47,35 +48,52 @@ const Circular: React.FC<CircularProps> = ({size = "medium", activity = "pills",
   const capPercentage = percentage > 100 ? 100 : (percentage < 0 ? 0 : percentage);
   const offset = circumference - (capPercentage / 100) * circumference;
 
-  return (
+    // Definir los colores del gradiente para cada actividad
+    const gradientId = `gradient-${activity}`;
+    const gradientColors = {
+        pills: ["#E0E030", "#7A7A1A"],
+        salad: ["#28C40B", "#135E05"],
+        steps: ["#EDB14D", "#87652C"],
+        water: ["#33D4ED", "#1D7987"],
+    };
+    const [startColor, endColor] = gradientColors[activity];
+
+
+    return (
     <div className={`${sizeClass} relative flex items-center justify-center`}>
-      <svg className="w-full h-full" viewBox="0 0 120 120">
-        <circle
-          className={innerCircle({activity}) + " fill-secondary-100"}
-          strokeWidth="8"
-          stroke="currentColor"
-          fill="secondary-100"
-          r="50"
-          cx="60"
-          cy="60"
-        />
-        <circle
-          className={circular({activity})}
-          strokeWidth={percentage >= 100 ? 12 : 8}
-          stroke="currentColor"
-          fill="transparent"
-          r="50"
-          cx="60"
-          cy="60"
-          strokeDasharray={circumference}
-          strokeDashoffset={offset}
-          strokeLinecap="round"
-          transform="rotate(-90 60 60)"
-        />
-      </svg>
-      <div className="absolute flex items-center justify-center">
-        {children}
-      </div>
+        <svg className="w-full h-full" viewBox="0 0 120 120">
+            <defs>
+                <linearGradient id={gradientId} x1="100%" y1="0%" x2="0%" y2="100%">
+                    <stop offset="0%" stopColor={startColor}/>
+                    <stop offset="100%" stopColor={endColor}/>
+                </linearGradient>
+            </defs>
+            <circle
+                className={innerCircle({activity}) + " fill-secondary-100"}
+                strokeWidth="8"
+                stroke="currentColor"
+                fill="secondary-100"
+                r="50"
+                cx="60"
+                cy="60"
+            />
+            <circle
+                className={circular({activity})}
+                strokeWidth={percentage >= 100 ? 12 : 8}
+                stroke={`url(#${gradientId})`}
+                fill="transparent"
+                r="50"
+                cx="60"
+                cy="60"
+                strokeDasharray={circumference}
+                strokeDashoffset={offset}
+                strokeLinecap="round"
+                transform="rotate(-90 60 60)"
+            />
+        </svg>
+        <div className="absolute flex items-center justify-center">
+            {children}
+        </div>
     </div>
   );
 }
