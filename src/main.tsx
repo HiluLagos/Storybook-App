1c02d1bd89074af3e8bf232b4b3458c4f70f1b2c
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-11-12 17:25:10 -0300

    Added main redirects

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './index.css';
 import TasksView from "./views/tasks/TasksView.tsx";
 import Activities from "./views/activities/Activities.tsx";
 
+const App = () => (
+  <div className="flex flex-col items-center app gap-4">
+    <h1>Welcome</h1>
+    <div className="buttons flex flex-col gap-2 p-4 bg-gray-200 rounded items-center">
+      <Link to="/activities">
+        <button className="bg-blue-500 text-white p-2 rounded">Go to Activities</button>
+      </Link>
+      <Link to="/tasks">
+        <button className="bg-green-500 text-white p-2 rounded">Go to Tasks</button>
+      </Link>
+    </div>
+  </div>
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Activities />
-    <TasksView />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/tasks" element={<TasksView />} />
+      </Routes>
+    </Router>
   </StrictMode>
-)
+);
